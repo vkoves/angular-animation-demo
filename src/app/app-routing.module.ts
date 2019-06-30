7c05a6098cd27dd0bee7b88579b5d90db904e955
Viktor Köves <3187531+vkoves@users.noreply.github.com>
2019-06-30 15:58:09 -0500

    Setup app basics with styles, header, and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [{
+  path: '',
+  pathMatch: 'full',
+  component: HomeComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
